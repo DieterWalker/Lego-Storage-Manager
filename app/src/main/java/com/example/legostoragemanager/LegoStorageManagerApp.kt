67224efb356c4ca5697e95a80fd276669357ba6f
Dieter Walker <nguyenquocdue32@gmail.com>
2026-02-05 18:02:54 +0700

    commit 05/02

--- app/src/main/java/com/example/legostoragemanager/LegoStorageManagerApp.kt
+++ app/src/main/java/com/example/legostoragemanager/LegoStorageManagerApp.kt
@@ -1,2 +1,16 @@
 package com.example.legostoragemanager
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.legostoragemanager.ui.navigation.InventoryNavHost
+
+@Composable
+fun LegoStorageManagerApp(navController: NavHostController = rememberNavController()){
+    InventoryNavHost(
+        navController = navController,
+        modifier = Modifier
+    )
+}
--- app/src/main/java/com/example/legostoragemanager/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/legostoragemanager/ui/home/HomeScreen.kt
@@ -1,12 +1,18 @@
 package com.example.legostoragemanager.ui.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.legostoragemanager.R
 import com.example.legostoragemanager.ui.navigation.NavigationDestination
 
@@ -18,13 +24,62 @@ object HomeDestination : NavigationDestination{
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
+    navigateToPiecesScreen: () -> Unit,
+    navigateToMinifigureScreen: () -> Unit,
     modifier: Modifier = Modifier
 ){
+    Scaffold(
+
+    ) { innerPadding ->
+        HomeBody(
+            navigateToPiecesScreen = navigateToPiecesScreen,
+            navigateToMinifigureScreen = navigateToMinifigureScreen,
+            modifier = modifier,
+            contentPadding = innerPadding,
+        )
+    }
+}
+
+@Composable
+fun HomeTopBar(
+    modifier: Modifier = Modifier
+){
+
+}
+@Composable
+fun HomeBody(
+    navigateToPiecesScreen: () -> Unit,
+    navigateToMinifigureScreen: () -> Unit,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(all = 0.dp),
+){
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = "Welcome Back!"
+        )
+        Button(
+            onClick = navigateToPiecesScreen,
+        ) {
+            Text("Brick")
+        }
+
+        Button(
+            onClick = navigateToPiecesScreen
+        ) {
+            Text("Minifigure")
+        }
+    }
 
 }
 
 @Preview
 @Composable
 fun HomeScreenPreview(){
-    HomeScreen()
+    HomeScreen(
+        navigateToPiecesScreen = {} ,
+        navigateToMinifigureScreen = {},
+        modifier = Modifier.fillMaxSize(),
+    )
 }
